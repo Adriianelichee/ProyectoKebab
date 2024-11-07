1c61a03df91eb189667116c8a9159cccdb563ef5
Adrian <treniyt@gmail.com>
2024-11-07 15:11:45 +0100

    Kebab Repos V1

---
+++ Repositorios/RepoOrderLine.php
@@ -0,0 +1,37 @@
+<?php
+
+class RepoOrderLine implements RepoCrud{
+    private $conexion;
+
+    public function __construct($conexion){
+        $this->conexion = $conexion;
+    }
+    public function create($OrderLin) {
+        // Implementación del método create
+        
+    }
+
+    public function update($id, $obj) {
+        // Implementación del método update
+    }
+
+    public function delete($id) {
+        // Implementación del método delete
+    }
+
+    public function getById($id) {
+        // Implementación del método getById
+    }
+
+    public function getAll() {
+        // Implementación del método getAll
+    }
+
+    public function find($criterio) {
+        // Implementación del método find
+    }
+
+    public function count() {
+        // Implementación del método count
+    }
+}
